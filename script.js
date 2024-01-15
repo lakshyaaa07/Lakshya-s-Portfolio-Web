1f90e1e9785ee7c68b5639a8b23a9dc3d327bafe
Lakshyaaa <112760813+lakshyaaa07@users.noreply.github.com>
2024-01-15 16:42:45 +0530

    responsiveness done!!!!!!!

--- script.js
+++ script.js
@@ -118,3 +118,40 @@ tl.to("#imgTwo",{
 .to(".overlay",{
     opacity: 1,
 }, 'sa')
+
+
+// toggle icon navbar  //////////////////////////////////////////////////////////////////
+
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+window.onscroll = ()=>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
+
+// parallax  //////////////////////////////////////////////////////////////////
+
+const obeserver = new IntersectionObserver((enteries)=>{
+    enteries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        }else {
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>obeserver.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>obeserver.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>obeserver.observe(el));
